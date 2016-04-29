90008199a5c020c0fa6d361a5fc6991857b9be4e
Patrick Best <patrickbest@patrickbest.com>
2016-04-29 14:01:43 -0500

    add url to data function
    so we can not corrupt our canvas adding remote urls

--- js/main.js
+++ js/main.js
@@ -123,6 +123,26 @@ Generator = {
 
   },
 
+  urlToData: function(url, callback, outputFormat) {
+    var img = new Image();
+
+    img.crossOrigin = 'Anonymous';
+    img.onload = function(){
+      var canvas = document.createElement('converter');
+      var ctx = canvas.getContext('2d');
+      var dataURL;
+
+      canvas.height = this.height;
+      canvas.width = this.width;
+      ctx.drawImage(this, 0, 0);
+      dataURL = canvas.toDataURL(outputFormat);
+      callback(dataURL);
+      canvas = null;
+    };
+
+    img.src = url;
+}
+
 };
 
 jQuery(document).ready(function () {
